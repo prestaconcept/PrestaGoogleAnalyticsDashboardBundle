f636ee2191ea281c93ca391bb3e997c8c7f6f0d7
Nicolas Bastien <nbastien@prestaconcept.net>
2013-10-23 12:49:33 +0200

    Set default layout

--- Controller/Admin/DefaultController.php
+++ Controller/Admin/DefaultController.php
@@ -25,7 +25,16 @@ class DefaultController extends AdminController
      */
     public function indexAction()
     {
-        $viewParams = array();
+        $viewParams = array(
+            'today_visit' => 'todo',
+            'today_page_view' => 'todo',
+            'today_page_per_visit' => 'todo',
+            'today_avg_time_on_site' => 'todo',
+            'yesterday_visit' => 'todo',
+            'yesterday_page_view' => 'todo',
+            'yesterday_page_per_visit' => 'todo',
+            'yesterday_avg_time_on_site' => 'todo',
+        );
 
         return $this->renderResponse('PrestaGoogleAnalyticsDashboardBundle:Admin/Default:index.html.twig', $viewParams);
     }
